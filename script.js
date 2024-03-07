1115012112978fe9ebfe0bcb79c249096ac920a1
Mahicol <377markin@gmail.com>
2024-03-07 15:41:17 -0500

    Agregando archivos HTML, CSS y JavaScript

---
+++ script.js
@@ -0,0 +1,11 @@
+function cotizarComida() {
+    window.location.href = "https://wa.me/3183613628"; // Reemplaza xxxxxxxxxx con tu número de WhatsApp
+}
+
+function asesoramientoWhatsApp() {
+    window.location.href = "https://wa.me/3183613628"; // Reemplaza xxxxxxxxxx con tu número de WhatsApp
+}
+
+function verMenu() {
+    document.getElementById("menu").scrollIntoView({ behavior: "smooth" });
+}
